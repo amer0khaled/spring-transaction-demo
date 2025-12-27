c04e5c074dc4ff551f9a71b9a67e01d84806f083
Amer Khaled <amrkhald144@gmail.com>
2025-12-27 14:40:30 +0200

    demo for transaction management in spring boot

---
+++ src/main/java/com/amer/transactionsdemo/TransactionsDemoApplication.java
@@ -0,0 +1,13 @@
+package com.amer.transactionsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionsDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TransactionsDemoApplication.class, args);
+    }
+
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+TRUNCATE TABLE audit_logs;
+TRUNCATE TABLE accounts;
+
+INSERT INTO accounts (owner, balance) VALUES ('mohamed', 2000);
+INSERT INTO accounts (owner, balance) VALUES ('ali', 500);
